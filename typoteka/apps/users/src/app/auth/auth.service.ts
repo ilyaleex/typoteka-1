33d7b12f5151ab69ea6466ecf6892bebcb7b6ade
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-16 23:21:55 +0300

    2.22. Добавит метод для авторизации пользователя в `AuthService`.
    Аналогичным образом добавим в сервис `AuthService` методы `verifyUser`
    и `getUser`. В первом выполним проверку логина и пароля
    пользователя. Для этого соберём сущность пользователя и воспользуемся
    методом `comparePassword`.
    
    В методе `getUser` реализуем получение пользователя по уникальному
    идентификатору.

--- typoteka/apps/users/src/app/auth/auth.service.ts
+++ typoteka/apps/users/src/app/auth/auth.service.ts
@@ -3,8 +3,9 @@ import * as dayjs from 'dayjs';
 import { UserRole } from '@typoteka/shared-types';
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
 import { CreateUserDto } from './dto/create-user.dto';
-import { AUTH_USER_EXISTS } from './auth.constant';
+import { AUTH_USER_EXISTS, AUTH_USER_NOT_FOUND, AUTH_USER_PASSWORD_WRONG } from './auth.constant';
 import { BlogUserEntity } from '../blog-user/blog-user.entity';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class AuthService {
@@ -32,6 +33,26 @@ export class AuthService {
 
     return this.blogUserRepository
       .create(userEntity);
+  }
+
+  async verifyUser(dto: LoginUserDto) {
+    const {email, password} = dto;
+    const existUser = await this.blogUserRepository.findByEmail(email);
+
+    if (!existUser) {
+      throw new Error(AUTH_USER_NOT_FOUND);
+    }
+
+    const blogUserEntity = new BlogUserEntity(existUser);
+    if (! await blogUserEntity.comparePassword(password)) {
+      throw new Error(AUTH_USER_PASSWORD_WRONG);
+    }
+
+    return blogUserEntity.toObject();
+  }
+
+  async getUser(id: string) {
+    return this.blogUserRepository.findById(id);
   }
 
 }
