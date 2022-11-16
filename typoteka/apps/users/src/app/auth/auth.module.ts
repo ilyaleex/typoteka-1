94ac816f3377f268bb3e8fa83dc6a72c84955528
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-16 23:04:41 +0300

    2.17. Добавит импорт модуля `BlogUserModule`.
    Переходим ко второму модулю — `AuthModule`. В рамках этого модуля
    реализуем контроллер (он будет обрабатывать запросы пользователей) и
    сервис (взаимодействует с репозиторием).
    
    Поскольку `AuthModule` потребуется взаимодействовать с репозиторием,
    который определён в другом модуле, необходимо импортировать нужный
    модуль.
    
    Обратите внимание, мы планируем использовать провайдер
    `BlogUserMemoryRepository`, но выполняем импорт именно модуля
    — `BlogUserModule`. Мы импортируем модуль, но использовать будем
    провайдер.

--- typoteka/apps/users/src/app/auth/auth.module.ts
+++ typoteka/apps/users/src/app/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { BlogUserModule } from '../blog-user/blog-user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [BlogUserModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
