8a038ca498cdc6ec3ca3460f135df0df6a6ecaaf
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-16 23:18:06 +0300

    2.21. Добавит метод регистрации пользователя в сервис `AuthService`.
    Для регистрации пользователей определим новый метод в сервисе
    `AuthService` и напишем в нём всё необходимую логику.
    
    Метод `register` принимает аргументом `dto` типа `CreateUserDto`.
    Внутри метода извлекаем все необходимые поля, а затем начинаем
    взаимодействовать с репозиторием.
    
    Перед добавлением информации о новом пользователе, выполним проверку.
    В базе не может быть двух пользователей с одинаковым email.

--- typoteka/apps/users/src/app/auth/auth.service.ts
+++ typoteka/apps/users/src/app/auth/auth.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import * as dayjs from 'dayjs';
+import { UserRole } from '@typoteka/shared-types';
 import { BlogUserMemoryRepository } from '../blog-user/blog-user-memory.repository';
+import { CreateUserDto } from './dto/create-user.dto';
+import { AUTH_USER_EXISTS } from './auth.constant';
+import { BlogUserEntity } from '../blog-user/blog-user.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly blogUserRepository: BlogUserMemoryRepository
   ) {}
+
+  async register(dto: CreateUserDto) {
+    const {email, firstname, lastname, password, dateBirth} = dto;
+    const blogUser = {
+      email, firstname, lastname, role: UserRole.User,
+      avatar: '', dateBirth: dayjs(dateBirth).toDate(),
+      passwordHash: ''
+    };
+
+    const existUser = await this.blogUserRepository
+      .findByEmail(email);
+
+    if (existUser) {
+      throw new Error(AUTH_USER_EXISTS);
+    }
+
+    const userEntity = await new BlogUserEntity(blogUser)
+      .setPassword(password)
+
+    return this.blogUserRepository
+      .create(userEntity);
+  }
+
 }
