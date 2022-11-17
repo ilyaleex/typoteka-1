89b7853d63282b354f8bc8ee02d9b443e2d4a82a
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-17 12:33:21 +0300

    2.26. Добавит обработчики маршрутов `/login`, `/:id`.
    Аналогичным образом добавим обработчики для маршрутов `/login`,
    `/:id`. Внутри обработчиков происходит обращение к соответствующим
    методом сервиса `AuthService`.

--- typoteka/apps/users/src/app/auth/auth.controller.ts
+++ typoteka/apps/users/src/app/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { fillObject } from '@typoteka/core';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { LoggedUserRdo } from './rdo/logged-user.rdo';
 import { UserRdo } from './rdo/user.rdo';
 
 @Controller('auth')
@@ -15,4 +17,17 @@ export class AuthController {
     const newUser = await this.authService.register(dto);
     return fillObject(UserRdo, newUser);
   }
+
+  @Post('login')
+  async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.authService.verifyUser(dto);
+    return fillObject(LoggedUserRdo, verifiedUser);
+  }
+
+  @Get(':id')
+  async show(@Param('id') id: string) {
+    const existUser = await this.authService.getUser(id);
+    return fillObject(UserRdo, existUser);
+  }
+
 }
