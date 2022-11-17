4379ac22fc701cc9fa50f455429e67570b179287
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-17 19:25:42 +0300

    2.33. Детализирует ответы обработчиков маршрутов. `@ApiResponse`.
    С помощью декоратора `@ApiResponse` можно детализировать ответ:
    добавить описание, код состояния и так далее.

--- typoteka/apps/users/src/app/auth/auth.controller.ts
+++ typoteka/apps/users/src/app/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
 import { ApiTags, ApiResponse } from '@nestjs/swagger';
 import { fillObject } from '@typoteka/core';
 import { AuthService } from './auth.service';
@@ -15,14 +15,25 @@ export class AuthController {
   ) {}
 
   @Post('register')
+  @ApiResponse({
+    status: HttpStatus.CREATED,
+    description: 'The new user has been successfully created.'
+  })
   async create(@Body() dto: CreateUserDto) {
     const newUser = await this.authService.register(dto);
     return fillObject(UserRdo, newUser);
   }
 
   @Post('login')
+  @HttpCode(HttpStatus.OK)
   @ApiResponse({
     type: LoggedUserRdo,
+    status: HttpStatus.OK,
+    description: 'User has been successfully logged.'
+  })
+  @ApiResponse({
+    status: HttpStatus.UNAUTHORIZED,
+    description: 'Password or Login is wrong.',
   })
   async login(@Body() dto: LoginUserDto) {
     const verifiedUser = await this.authService.verifyUser(dto);
@@ -32,6 +43,8 @@ export class AuthController {
   @Get(':id')
   @ApiResponse({
     type: UserRdo,
+    status: HttpStatus.OK,
+    description: 'User found'
   })
   async show(@Param('id') id: string) {
     const existUser = await this.authService.getUser(id);
