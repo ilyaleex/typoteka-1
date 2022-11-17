0e91452a2e6f8f6820f993e38c3eb5f485b1bfb0
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-17 18:26:17 +0300

    2.29. Добавит базовую настройку `@nestjs/swagger`.
    Выполним базовую настройку модуля `@nestjs/swagger`. В точке входа
    (`main.ts`) добавим инициализацию `DocumentBuilder` и
    `SwaggerModule`.
    
    Укажем заголовок (`setTitle`), описание (`setDescription`),
    версия (`setVersion`).
    
    Последним шагом подготовим настройки для `SwaggerModule`. Аргументом
    передадим путь (`spec`) по которому будет доступна спецификация.
    
    Попробуйте запустить проект и обратиться к `http://localhost:3333/spec`.
    Вы спецификацию API, сгенерированную модулем `Swagger`.

--- typoteka/apps/users/src/main.ts
+++ typoteka/apps/users/src/main.ts
@@ -5,13 +5,25 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('The «Users» service')
+    .setDescription('Users service API')
+    .setVersion('1.0')
+    .build();
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('spec', app, document)
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
