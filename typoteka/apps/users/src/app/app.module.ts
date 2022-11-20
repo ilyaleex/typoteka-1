64a45fff288577331def0fc097fb62ae5bb6d091
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-20 15:38:29 +0300

    3.3. Подключит модуль для работы с конфигурацией приложения.
    Подготовительные действия завершены, можно переходить к практической
    стороне. Чтобы воспользоваться модулем `ConfigModule`, его необходимо
    подключить в один из модулей приложения.
    
    Чаще всего с конфигурацией приложения приходится работать глобально.
    Доступ к определённым параметрам может потребоваться в разных частях
    приложения. Поэтому `ConfigModule` мы подключим в корневой модуль
    приложения.
    
    Для подключения модуля необходимо его импортировать. Пропишем его в
    секции `imports` глобального модуля (`AppModule`).
    
    Импортировать модуль можно как `forRoot` или `forFeature`. Мы используем
    модуль глобально для всего приложения, поэтому воспользуемся `forRoot`.
    
    В метод `forRoot` мы передаём объект настроек типа `configModuleOptions`.
    Из настроек нас интересуют:
    
    * `cache` — требуется ли кэшировать считанные настройки в памяти. В
    определённых случаях это может улучшить производительность.
    
    * `isGlobal` — признак, регистрации модуля в качестве глобального.
    Этот флаг избавит нас от необходимости импортировать и регистрировать
    `ConfigModule` в других модулях, где планируется его использовать.
    
    * `envFilePath` — путь к `.env` файлу, который необходимо прочитать и
    загрузить. Опция необязательна. Если её не передать, то подразумевается,
    что файл хранится в корневой директории проекта. Мы договорились все
    подобные файлы хранить в отдельной директории `environments` поэтому
    укажем путь здесь.
    
    Всё, этого достаточно чтобы начать работать с конфигурацией приложения
    через модуль `ConfigModule`. Например, чтобы воспользоваться модулем в
    сервисе или контроллере, достаточно объявить соответствующий параметр в
    конструкторе. Детали внедрения `ConfigModule` в качестве зависимости
    Nest возьмёт на себя.

--- typoteka/apps/users/src/app/app.module.ts
+++ typoteka/apps/users/src/app/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_FILE_PATH } from './app.constant';
 
 @Module({
-  imports: [AuthModule, BlogUserModule],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      envFilePath: ENV_FILE_PATH,
+    }),
+    AuthModule,
+    BlogUserModule
+  ],
   controllers: [],
   providers: [],
 })
