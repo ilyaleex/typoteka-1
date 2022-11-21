8c5a62bb6b6b456d1df4a2c677ad6d479a130dde
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-21 19:15:57 +0300

    3.4. Добавит для конфигурации пространство имён `database`.
    Чем больше функциональности предоставляет приложение, тем больше
    входных данных ему может потребоваться. Модуль для управления
    конфигурацией позволяет загружать несколько пользовательских файлов
    конфигурации. Чтобы проще взаимодействовать с загруженными настройками,
    их можно разделить по пространствам имён. Проще говоря: сгруппировать.
    
    Для этого необходимо воспользоваться фабрикой `registerAs`. Технически
    это обычная функция, которая должна вернуть объект с настройками,
    который будет закреплён за определённым пространством имён. Рассмотрим
    пример создания пространства имён `database`. Опишем функцию
    `registerAs` в отдельном модуле `database.config.ts`.
    
    Аргументом функции передадим название нового пространства
    имён — `database`. Именно через это имя мы будем обращаться для
    получения настроек соединения с базой данных.
    
    Вторым аргументом функция принимает колбэк. Он должен вернуть объект
    с настройками. Для получения настроек обратимся к объекту `process.env`.
    Здесь же мы можем предусмотреть настройки по умолчанию, выполнить
    преобразование типов (не забывайте, настройки изначально представлены
    в виде строк).
    
    Теперь воспользуемся подготовленной функцией. В главном модуле
    приложения (`app.module.ts`) при подготовке модуля конфигурации
    воспользуемся ключом `load` и передадим результат выполнения функции.

--- typoteka/apps/users/src/app/app.module.ts
+++ typoteka/apps/users/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { AuthModule } from './auth/auth.module';
 import { BlogUserModule } from './blog-user/blog-user.module';
 import { ConfigModule } from '@nestjs/config';
 import { ENV_FILE_PATH } from './app.constant';
+import databaseConfig from '../config/database.config';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ENV_FILE_PATH } from './app.constant';
       cache: true,
       isGlobal: true,
       envFilePath: ENV_FILE_PATH,
+      load: [databaseConfig]
     }),
     AuthModule,
     BlogUserModule
