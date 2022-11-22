fc7bd42978188ca5adbc4de2d30e959e6e1f796c
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-22 12:21:37 +0300

    3.15. Добавит функцию `getMongoConnectionString`.
    Для установки соединения с MongoDB нам потребуется подготовленная строка
    соединения (connection string). В этой строке содержится информация о
    подключении к СУБД: протокол, имя пользователя, пароль, адрес хоста и
    другие необходимые параметры.
    
    Использовать MongoDB могут несколько сервисов, поэтому разумно
    подготовить отдельную функцию `getMongoConnectionString` и разместить
    её в общей библиотеке. Для этой цели хорошо подойдёт ранее созданный
    модуль `helpers.ts`.
    
    Функция для формирования строки подключения реализуется максимально
    просто: на вход принимает все необходимые значения, а затем, используя
    интерполяцию, формирует строку для подключения.

--- typoteka/libs/core/src/lib/helpers.ts
+++ typoteka/libs/core/src/lib/helpers.ts
@@ -3,3 +3,7 @@ import {plainToInstance, ClassConstructor} from 'class-transformer';
 export function fillObject<T, V>(someDto: ClassConstructor<T>, plainObject: V) {
   return plainToInstance(someDto, plainObject, {excludeExtraneousValues: true});
 }
+
+export function getMongoConnectionString({username, password, host, port, databaseName, authDatabase}): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+}
