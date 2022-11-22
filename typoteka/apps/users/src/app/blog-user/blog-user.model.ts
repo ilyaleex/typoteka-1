da7035a78a4985ae08d6eaedec3404fb931d0f94
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-22 14:26:08 +0300

    3.18. Добавит модель `BlogUserModel`.
    Настройки подключения завершены, теперь можно заняться подготовкой
    модели `BlogUserModel`. Здесь нам опять поможет пакет `@nestjs/mongoose`.
    Он предоставляет дополнительные инструменты, которые избавляют от
    необходимости написания бойлерплейт кода. Подход во многом напоминает
    применение пакета `typegoose`, который рассматривался в предыдущем курсе.
    
    Модель наследуется от класса `Document`. От него мы получим основные
    методы работы с моделью, также свойства.
    
    Мы также укажем, что модель реализует интерфейс `User`. Это делать
    необязательно, но таким образом мы добавим связь с основным интерфейсом.
    Если он измениться, TypeScript сразу сообщит, в каких частях приложения
    следует вносить изменения.
    
    Класс с моделью мы декорируем с помощью декоратора `@Schema`. Аргументом
    он принимает необязательный объект настроек. Подробно про него можно
    прочитать в документации к Mongoose. Мы воспользуемся только одним
    свойством — `collection`. В нём можно указать имя коллекции, которая
    будет хранит модели данного типа.
    
    Затем имплементируем интерфейс `User`: объявим все поля и
    задекорируем их с помощью декоратора `Prop`. Декоратор `@Prop` также
    принимает объект настроек. В нём можно указать правила валидации,
    используя встроенные валидаторы в Mongoose. На данном этапе мы
    воспользуемся только валидатором `required`, тем самым отметив поле
    обязательным для заполнения.
    
    Обратите внимание на определение поля `role`. Поскольку в качестве
    значения используется одно из значений перечисления, мы указываем
    признак, что это `enum`, а в качестве типа — `String`. Это означает,
    что внутри коллекции будут хранится строки: `user` или `admin`.
    
    Мы также задаём значение по умолчанию — `UserRole.User`. Всем
    зарегистрированным пользователям будет назначаться роль `User`.
    
    Последнее, что необходимо сделать: подготовить схему. Схема создаётся
    на основании модели. Для этого мы используем статический метод
    `createForClass` у класса `SchemaFactory`.

---
+++ typoteka/apps/users/src/app/blog-user/blog-user.model.ts
@@ -0,0 +1,47 @@
+import { Document } from 'mongoose';
+import { User, UserRole } from '@typoteka/shared-types';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+@Schema({
+  collection: 'users',
+})
+export class BlogUserModel extends Document implements User {
+  @Prop()
+  public avatar: string;
+
+  @Prop({
+    required: true,
+  })
+  public dateBirth: Date;
+
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  public email: string;
+
+  @Prop({
+    required: true,
+  })
+  public firstname: string;
+
+  @Prop({
+    required: true,
+  })
+  public lastname: string;
+
+  @Prop({
+    required: true,
+  })
+  public passwordHash: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRole,
+    default: UserRole.User,
+  })
+  public role: UserRole;
+}
+
+export const BlogUserSchema = SchemaFactory.createForClass(BlogUserModel);
