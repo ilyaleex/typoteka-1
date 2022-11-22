b4b313f08fa5f01db43c2d29642b2ae90f71ef82
Igor Antonov <antonov.igor.khv@gmail.com>
2022-11-22 14:40:52 +0300

    3.20. Подключит `MongooseModule` в модуль `BlogUserModule`.
    Предпоследним шагом подключим `MongooseModule` к модулю `BlogUserModule`
    и заменим `BlogUserMemoryRepository` на `BlogUserRepository`.

--- typoteka/apps/users/src/app/blog-user/blog-user.module.ts
+++ typoteka/apps/users/src/app/blog-user/blog-user.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
-import { BlogUserMemoryRepository } from './blog-user-memory.repository';
+import {MongooseModule} from '@nestjs/mongoose';
+import {BlogUserModel, BlogUserSchema} from './blog-user.model';
+import {BlogUserRepository} from './blog-user.repository';
 
 @Module({
-  providers: [BlogUserMemoryRepository],
-  exports: [BlogUserMemoryRepository]
+  imports: [MongooseModule.forFeature([
+    { name: BlogUserModel.name, schema: BlogUserSchema }
+  ])],
+  providers: [BlogUserRepository],
+  exports: [BlogUserRepository]
 })
 export class BlogUserModule {}
