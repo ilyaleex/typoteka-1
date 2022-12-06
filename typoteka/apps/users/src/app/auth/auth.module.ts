b45827da1fa7292e8e428aba47dd2d5b24c4b801
Igor Antonov <antonov.igor.khv@gmail.com>
2022-12-06 17:35:06 +0300

    5.14. Добавит подключение модуля `JwtModule` в `AuthModule.`

--- typoteka/apps/users/src/app/auth/auth.module.ts
+++ typoteka/apps/users/src/app/auth/auth.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { BlogUserModule } from '../blog-user/blog-user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getJwtConfig } from '../../config/jwt.config';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
-  imports: [BlogUserModule],
+  imports: [
+    BlogUserModule,
+    PassportModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getJwtConfig
+    })
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
